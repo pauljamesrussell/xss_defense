756660631940a735430c64fb9f32068a16da72d6
Paul Russell <paul@pauljamesrussell.org>
2011-06-28 23:40:23 +0100

    Added check that the table exists.

--- lib/xss_defense/active_record.rb
+++ lib/xss_defense/active_record.rb
@@ -8,8 +8,12 @@ module XssDefense
     
     module ClassMethods
       def xss_defense
-        cols = self.columns.select { |c| c.type == :string || c.type == :text }
-        validates_with ::XssDefense::XssValidator, :attributes => cols.map {|c| c.name.to_sym}
+        if table_exists?
+          cols = self.columns.select { |c| c.type == :string || c.type == :text }
+          validates_with ::XssDefense::XssValidator, :attributes => cols.map {|c| c.name.to_sym}
+        else
+          logger.warn "Could not enable xss_defense on #{name}, as its underlying table didn't exist."
+        end
       end
     end
     
